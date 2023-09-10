3e9979bab94fdfb7980a696d0d01c9c8a19452f2
ShewaniKunal <kunalshewani0@gmail.com>
2023-09-10 10:44:33 +0530

    first commit

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ public class hello {
 public static void main(String[] args) {
     System.out.println("HI");
     System.out.println("GIT");
+    System.out.println("Bye GIT");
+    System.out.println("updated");
 }
 }
